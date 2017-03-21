7ed7690143e152abdb8e8d3e019e196b371d7bcf
Marek Urban <umarekk@gmail.com>
2017-03-21 22:04:26 +0100

    Add PlayerController; update Direction with direction multipliers

--- GameEngine/src/main/java/cz/muni/fi/pv260/control/direction/Direction.java
+++ GameEngine/src/main/java/cz/muni/fi/pv260/control/direction/Direction.java
@@ -1,9 +1,47 @@
 package cz.muni.fi.pv260.control.direction;
 
+/**
+ * @author <a href="mailto:umarekk@gmail.com">Marek Urban</a>
+ */
 public enum Direction {
+    DIRECTION_UP,
+    DIRECTION_RIGHT,
+    DIRECTION_DOWN,
+    DIRECTION_LEFT;
 
-    DOWN,
-    LEFT,
-    UP,
-    RIGHT
+    private int MULTIPLIER_X;
+    private int MULTIPLIER_Y;
+
+    private Direction OPPOSITE;
+
+    static {
+        DIRECTION_UP.MULTIPLIER_X = 0;
+        DIRECTION_UP.MULTIPLIER_Y = -1;
+
+        DIRECTION_RIGHT.MULTIPLIER_X = 1;
+        DIRECTION_RIGHT.MULTIPLIER_Y = 0;
+
+        DIRECTION_DOWN.MULTIPLIER_X = 0;
+        DIRECTION_DOWN.MULTIPLIER_Y = 1;
+
+        DIRECTION_LEFT.MULTIPLIER_X = -1;
+        DIRECTION_LEFT.MULTIPLIER_Y = 0;
+
+        DIRECTION_UP.OPPOSITE = DIRECTION_DOWN;
+        DIRECTION_RIGHT.OPPOSITE = DIRECTION_LEFT;
+        DIRECTION_DOWN.OPPOSITE = DIRECTION_UP;
+        DIRECTION_LEFT.OPPOSITE = DIRECTION_RIGHT;
+    }
+
+    public int getMultiplierX(){
+        return MULTIPLIER_X;
+    }
+
+    public int getMultiplierY(){
+        return MULTIPLIER_Y;
+    }
+
+    public Direction getOpposite(){
+        return OPPOSITE;
+    }
 }
---
+++ Tron/src/main/java/cz/muni/fi/pv260/controller/PlayerController.java
@@ -0,0 +1,40 @@
+package cz.muni.fi.pv260.controller;
+
+import cz.muni.fi.pv260.control.collision.Point;
+import cz.muni.fi.pv260.model.Player;
+import java.awt.Window;
+
+/**
+ * @author <a href="mailto:umarekk@gmail.com">Marek Urban</a>
+ */
+public class PlayerController {
+
+    private static final int MOVE_LENGTH = 5;
+    private Window window;
+
+    public PlayerController(Window window) {
+        this.window = window;
+    }
+
+    public void move(Player player) {
+        int moveX = player.getDirectionControl().getDirection().getMultiplierX() * MOVE_LENGTH;
+        int moveY = player.getDirectionControl().getDirection().getMultiplierY() * MOVE_LENGTH;
+        adjustPosition(player, moveX, moveY);
+    }
+
+    private void adjustPosition(Player player, int moveX, int moveY) {
+        Point position = player.getPosition();
+        int coordinateX = position.getCoordinateX() + moveX;
+        int coordinateY = position.getCoordinateY() + moveY;
+
+        coordinateX = coordinateX > window.getWidth() ? 0 : coordinateX;
+        coordinateX = coordinateX < 0 ? window.getWidth() : coordinateX;
+
+        coordinateY = coordinateY > window.getHeight() ? 0 : coordinateY;
+        coordinateY = coordinateY < 0 ? window.getHeight() : coordinateY;
+
+        player.getPath().addPointToPath(new Point(coordinateX, coordinateY));
+    }
+
+
+}
