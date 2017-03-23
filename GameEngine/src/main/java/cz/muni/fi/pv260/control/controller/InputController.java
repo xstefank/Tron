7bd18375dafb1e1325b9c480907410b012b59af1
Marek Urban <umarekk@gmail.com>
2017-03-23 23:53:00 +0100

    Modify interface InputController

--- GameEngine/src/main/java/cz/muni/fi/pv260/control/controller/InputController.java
+++ GameEngine/src/main/java/cz/muni/fi/pv260/control/controller/InputController.java
@@ -4,15 +4,25 @@ package cz.muni.fi.pv260.control.controller;
  * Interface for all input methods which can require interaction
  * in the game
  *
- * @param <T> input event type
+ * @param <T1> input event type
+ * @param <T2> input action type
  *
  * @author <a href="mailto:xstefank122@gmail.com">Martin Stefanko</a>
+ * @author <a href="mailto:umarekk@gmail.com">Marek Urban</a>
  */
-public interface InputController<T> {
+public interface InputController<T1, T2> {
 
     /**
      * Process the input event
      * @param event occured input event
      */
-    void processEvent(T event);
+    void processEvent(T1 event);
+
+    /**
+     * Registers the controller event with the defined user action
+     *
+     * @param event keyboard event
+     * @param action user action to be performed when event occurs
+     */
+    void registerEvent(T1 event, T2 action);
 }
