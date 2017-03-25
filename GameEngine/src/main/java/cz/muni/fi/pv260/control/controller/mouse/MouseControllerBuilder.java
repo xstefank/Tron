b33a4d602dea19d6014a588dfcab2a2479a5fe42
Marek Urban <umarekk@gmail.com>
2017-03-25 18:50:22 +0100

    add MouseController and MouseControllerBuilder to GameEngine

---
+++ GameEngine/src/main/java/cz/muni/fi/pv260/control/controller/mouse/MouseControllerBuilder.java
@@ -0,0 +1,27 @@
+package cz.muni.fi.pv260.control.controller.mouse;
+
+import cz.muni.fi.pv260.control.controller.InputAction;
+import cz.muni.fi.pv260.control.controller.InputControllerBuilder;
+
+import java.awt.event.MouseEvent;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @author <a href="mailto:umarekk@gmail.com">Marek Urban</a>
+ */
+public class MouseControllerBuilder implements InputControllerBuilder<InputAction<MouseEvent>> {
+
+    private Map<Integer, InputAction<MouseEvent>> buttonEvents = new HashMap<>();
+
+    @Override
+    public MouseControllerBuilder addEvent(int keyCode, InputAction<MouseEvent> action) {
+        buttonEvents.putIfAbsent(keyCode, action);
+        return this;
+    }
+
+    @Override
+    public MouseControllerImpl build() {
+        return new MouseControllerImpl(buttonEvents);
+    }
+}
---
+++ GameEngine/src/main/java/cz/muni/fi/pv260/control/controller/mouse/MouseControllerImpl.java
@@ -0,0 +1,33 @@
+package cz.muni.fi.pv260.control.controller.mouse;
+
+import cz.muni.fi.pv260.control.controller.InputAction;
+import cz.muni.fi.pv260.control.controller.InputController;
+
+import java.awt.event.MouseEvent;
+import java.util.Map;
+
+/**
+ * @author <a href="mailto:umarekk@gmail.com">Marek Urban</a>
+ */
+public class MouseControllerImpl implements InputController<MouseEvent> {
+
+    private Map<Integer, InputAction<MouseEvent>> buttonEvents;
+
+    public MouseControllerImpl(Map<Integer, InputAction<MouseEvent>> buttonEvents) {
+        this.buttonEvents = buttonEvents;
+    }
+
+    @Override
+    public void processEvent(MouseEvent event) {
+        InputAction<MouseEvent> action = buttonEvents.get(event.getButton());
+
+        if (action != null) {
+            action.executeAction(event);
+        }
+    }
+
+    @Override
+    public void registerEvent(MouseEvent event, InputAction<MouseEvent> action) {
+        buttonEvents.putIfAbsent(event.getButton(), action);
+    }
+}
