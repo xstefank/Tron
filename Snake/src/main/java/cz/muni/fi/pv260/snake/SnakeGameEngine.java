4a1e565165303e1b45ab65baef0e50af2b6a3dca
xstefank <mjolnir122@gmail.com>
2017-03-26 11:51:38 +0200

    add Snake module; prepare for Snake game development

---
+++ Snake/src/main/java/cz/muni/fi/pv260/snake/SnakeGameEngine.java
@@ -0,0 +1,11 @@
+package cz.muni.fi.pv260.snake;
+
+/**
+ * @author <a href="mailto:xstefank122@gmail.com">Martin Stefanko</a>
+ */
+public class SnakeGameEngine {
+
+    public static void main(String[] args) {
+        System.out.println("Snake game");
+    }
+}
